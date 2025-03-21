f977d63e1890d8c69ec81ee2ce1de60d67bbb7ec
poornima <reddypoornima683@gmail.com>
2025-03-21 17:24:51 +0530

    refactor: move Flask app logic from app.py to src/main.py

--- src/main.py
+++ src/main.py
@@ -1,39 +1,82 @@
+from flask import Flask, request, jsonify
+import os
+import base64
+import json
+import re
+import google.generativeai as genai
+from flask_cors import CORS
 from appwrite.client import Client
 from appwrite.services.users import Users
 from appwrite.exception import AppwriteException
-import os
 
-# This Appwrite function will be executed every time your function is triggered
+from .config import configure_app
+from .routes import register
+
+def create_app():
+    """Create and configure the Flask application."""
+    app = Flask(__name__)
+    CORS(app)  # Enable CORS for all routes
+    
+    # Configure the application
+    configure_app(app)
+    
+    # Register all routes
+    register.register_routes(app)
+    
+    return app
+
+app = create_app()
+
+# For local development
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# For Appwrite Functions
 def main(context):
-    # You can use the Appwrite SDK to interact with other services
-    # For this example, we're using the Users service
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-    users = Users(client)
-
-    try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
-
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+    """
+    Main entry point for Appwrite Functions.
+    This function processes the request through the Flask app.
+    """
+    # Get the path from the request
+    path = context.req.path or "/"
+    
+    # Set up the WSGI environment
+    environ = {
+        'wsgi.input': context.req.body,
+        'wsgi.errors': None,
+        'wsgi.version': (1, 0),
+        'wsgi.multithread': False,
+        'wsgi.multiprocess': False,
+        'wsgi.run_once': False,
+        'wsgi.url_scheme': 'https',
+        'REQUEST_METHOD': context.req.method,
+        'PATH_INFO': path,
+        'QUERY_STRING': context.req.query_string or "",
+        'CONTENT_TYPE': context.req.headers.get('content-type', ''),
+        'CONTENT_LENGTH': context.req.headers.get('content-length', ''),
+        'SERVER_NAME': 'appwrite-function',
+        'SERVER_PORT': '443',
+        'SERVER_PROTOCOL': 'HTTP/1.1',
+    }
+    
+    # Add HTTP headers
+    for key, value in context.req.headers.items():
+        key = key.upper().replace('-', '_')
+        if key not in ('CONTENT_TYPE', 'CONTENT_LENGTH'):
+            environ[f'HTTP_{key}'] = value
+    
+    # Define a function to capture the response
+    def start_response(status, headers):
+        context.res.status = int(status.split(' ')[0])
+        for header, value in headers:
+            context.res.headers[header] = value
+    
+    # Process the request through Flask
+    response_data = b''
+    for data in app(environ, start_response):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+        response_data += data
+    
+    context.res.body = response_data
+    return context.res
